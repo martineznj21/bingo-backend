448683f832fc8ba35fabe0608b15ca8e77a48920
martineznj21 <125171387+martineznj21@users.noreply.github.com>
2025-07-22 16:14:54 -0500

    Second commit

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 app.use(cors());
 app.use(express.json());
 
